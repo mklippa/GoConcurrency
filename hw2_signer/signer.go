bb6d1d1b6d28496e40131ff15eb7850ab0862256
Maxim Klippa <maxim.klippa@gmail.com>
2019-07-13 19:42:39 +0300

    t2 dust

--- hw2_signer/signer.go
+++ hw2_signer/signer.go
@@ -1,34 +1,63 @@
 package main
 
 import (
-	"fmt"
+	"runtime"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // сюда писать код
 func main() {
-	fmt.Println(CombineResults(MultiHash(SingleHash("0")), MultiHash(SingleHash("1"))))
+	runtime.GOMAXPROCS(0)
+	// fmt.Println(CombineResults(MultiHash(SingleHash("0")), MultiHash(SingleHash("1"))))
 }
 
 func ExecutePipeline(jobs ...job) {
-
-}
-
-func CombineResults(results ...string) string {
-	sort.Strings(results)
-	return strings.Join(results, "_")
-}
-
-func SingleHash(data string) string {
-	return DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
-}
-
-func MultiHash(data string) string {
-	var result string
-	for i := 0; i <= 5; i++ {
-		result += DataSignerCrc32(strconv.Itoa(i) + data)
+	wg := &sync.WaitGroup{}
+	in := make(chan interface{}, 0)
+	out := make(chan interface{}, 0)
+	for _, j := range jobs {
+		wg.Add(1)
+		go func(worker job, inCh, outCh chan interface{}) {
+			worker(inCh, outCh)
+			runtime.Gosched()
+			wg.Done()
+		}(j, in, out)
 	}
-	return result
+	wg.Done()
+}
+
+var CombineResults job = func(in, out chan interface{}) {
+	result := make([]string, 0)
+	for {
+		select {
+		case d := <-in:
+			data, _ := d.(string)
+			result = append(result, data)
+		}
+	}
+	sort.Strings(result)
+	out <- strings.Join(result, "_")
+}
+
+var SingleHash job = func(in, out chan interface{}) {
+	for d := range in {
+		data, _ := d.(string)
+		go func(data string) {
+			out <- DataSignerCrc32(data) + "~" + DataSignerCrc32(DataSignerMd5(data))
+		}(data)
+	}
+}
+
+var MultiHash job = func(in, out chan interface{}) {
+	for d := range in {
+		data, _ := d.(string)
+		var result string
+		for i := 0; i <= 5; i++ {
+			result += DataSignerCrc32(strconv.Itoa(i) + data)
+		}
+		out <- result
+	}
 }
