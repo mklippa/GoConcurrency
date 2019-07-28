9a2c3e24a34ce130a1e7bdb27b78afc3c9e80757
Maxim Klippa <maxim.klippa@gmail.com>
2019-07-28 22:46:23 +0300

    hw4 pt3 (unit-tests)

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"encoding/xml"
+	"fmt"
 	"io"
 	"io/ioutil"
 	"net/http"
@@ -11,6 +12,7 @@ import (
 	"strconv"
 	"strings"
 	"testing"
+	"time"
 )
 
 type Root struct {
@@ -92,7 +94,29 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 	offset, _ := strconv.Atoi(r.FormValue("offset"))
 	query := r.FormValue("query")
 	orderField := r.FormValue("order_field")
-	orderBy, _ := strconv.Atoi("order_by")
+	orderBy, _ := strconv.Atoi(r.FormValue("order_by"))
+
+	if offset == 504 {
+		time.Sleep(1 * time.Second)
+		// w.WriteHeader(http.StatusGatewayTimeout)
+		return
+	}
+
+	if offset == 13 {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "Houston, we have a problem")
+		return
+	}
+
+	if offset == 666 {
+		panic("Something went wrong")
+	}
+
+	if offset == 69 {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "wrong data")
+		return
+	}
 
 	// парсинг данных
 	dataset, err := ioutil.ReadFile("dataset.xml")
@@ -162,7 +186,7 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 
 	// пагинация
 	if offset >= len(rows) {
-		w.WriteHeader(http.StatusBadRequest)
+		w.WriteHeader(http.StatusInternalServerError)
 		io.WriteString(w, `{"Error": "OffsetOutOfRange"}`)
 		return
 	}
@@ -194,24 +218,279 @@ func SearchServer(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, string(s))
 }
 
+const validAccessToken = "test"
+
+func TestSearchServerLimitLessThan0(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit: -1,
+	})
+
+	if err == nil {
+		t.Error("Check that limit must be > 0")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerOffsetLessThan0(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Offset: -1,
+	})
+
+	if err == nil {
+		t.Error("Check that offset must be > 0")
+	}
+
+	ts.Close()
+}
+
 func TestSearchServer(t *testing.T) {
 	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
 
 	c := &SearchClient{
-		AccessToken: "",     //! что установить?
-		URL:         ts.URL, //! возможно, что-то не то установил
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
 	}
 
-	c.FindUsers(SearchRequest{})
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 0,
+	})
+
+	if err != nil {
+		t.Error("Unexpected error")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerWrongData(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 69,
+	})
+
+	if err == nil {
+		t.Error("Unexpected error")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServer666(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 666,
+	})
+
+	if err == nil {
+		t.Error("Houston, we have a problem")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerInvalidErrorJson(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 13,
+	})
+
+	if err == nil {
+		t.Error("Houston, we have a problem")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerLastPage(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 30,
+	})
+
+	if err != nil {
+		t.Error("Unexpected error")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerTimeoutServerError(t *testing.T) {
+	// ts := httptest.NewServer(http.TimeoutHandler(http.HandlerFunc(SearchServer), 20*time.Millisecond, "server timeout"))
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 504,
+	})
+
+	if err == nil {
+		t.Error("Timeout server error handling")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerInternalServerError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 100000,
+	})
+
+	if err == nil || err.Error() != "SearchServer fatal error" {
+		t.Error("Check 'SearchServer fatal error' handling")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerUnknownBadRequestError(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:   10,
+		Offset:  0,
+		OrderBy: 2,
+	})
+
+	if err == nil || err.Error() != "unknown bad request error: ErrorBadOrderBy" {
+		t.Error("Check handling of unknown bad request error")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerErrorBadOrderField(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	req := SearchRequest{
+		Limit:      10,
+		Offset:     0,
+		OrderField: "Gender",
+	}
+	_, err := c.FindUsers(req)
+
+	if err == nil || err.Error() != fmt.Sprintf("OrderFeld %s invalid", req.OrderField) {
+		t.Error("Check ErrorBadOrderField handling")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerLimit25(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: validAccessToken,
+		URL:         ts.URL,
+	}
+
+	res, err := c.FindUsers(SearchRequest{
+		Limit:  30,
+		Offset: 0,
+	})
+
+	if err != nil {
+		t.Error("Unexpected error")
+	}
+
+	if len(res.Users) > 25 {
+		t.Error("Limit should not exceed 25")
+	}
+
+	ts.Close()
+}
+
+func TestSearchServerBadAccessToken(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(SearchServer))
+
+	c := &SearchClient{
+		AccessToken: "invalid token",
+		URL:         ts.URL,
+	}
+
+	_, err := c.FindUsers(SearchRequest{
+		Limit:  10,
+		Offset: 0,
+	})
+
+	if err == nil || err.Error() != "Bad AccessToken" {
+		t.Error("Should be 'Bad AccessToken' error")
+	}
 
 	ts.Close()
 }
 
 /*
 Тест-кейсы:
-	- limit < 0 -> error
+	- ✔ limit < 0 -> error
 	- limit > 25 -> return 25 items
-	- offset < 0 -> error
+	- ✔ offset < 0 -> error
 	- (?) limit is not int -> panic
 	- (?) offset is not int -> panic
 	- (?) order_by is not int -> panic
